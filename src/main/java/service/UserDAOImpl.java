74bb8bcc70bb39d09baad9692d034509cc932ed9
Kamito <Kamito062004@gmail.com>
2023-10-18 10:36:22 +0700

    create base project

---
+++ src/main/java/service/UserDAOImpl.java
@@ -0,0 +1,23 @@
+package service;
+
+import model.User;
+
+import java.util.List;
+
+public class UserDAOImpl implements IUserDAO{
+    @Override
+    public List<User> getAllUser() {
+        return null;
+    }
+
+    @Override
+    public User getUserById(int id) {
+        return null;
+    }
+
+    @Override
+    public void updateUser(User user) {
+
+    }
+
+}
