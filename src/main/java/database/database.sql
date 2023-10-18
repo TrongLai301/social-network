40b3401180d4d8472eddd5462356a9210a9d045d
Kamito <Kamito062004@gmail.com>
2023-10-18 14:32:41 +0700

    create database and add jstl and jdbc

---
+++ src/main/java/database/database.sql
@@ -0,0 +1,15 @@
+create database SocialNetwork ;
+use SocialNetwork;
+create table userAccount(
+idAccount int auto_increment primary key,
+username varchar(50) not null,
+password varchar(32) not null,
+CONSTRAINT CHK_PasswordLength CHECK (length(password) >= 6 AND length(password)<= 32),
+permision int,
+foreign key(permision) references permision(idPermision)
+);
+create table permision(
+idPermision int auto_increment primary key,
+namePermision varchar(20) not null
+);
+insert into userAccount(username,password,permision) value ("user","123456",2);
