d39a4ea55ed2a2c833ff5b0a5f5315068200bbdc
elspycongry <vuthanhtungcv1@gmail.com>
2023-10-18 13:48:04 +0700

    test

---
+++ src/main/java/com/example/socialnetwork/Modal/User.java
@@ -0,0 +1,16 @@
+package com.example.socialnetwork.Modal;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class User {
+    private int userId;
+    private String username;
+    private String password;
+
+    private String email;
+    private int phone;
+    private Date birthday;
+
+
+}
