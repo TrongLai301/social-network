764a296b9b21fbab51df79ca6e81fd53cae6a8d4
elspycongry <vuthanhtungcv1@gmail.com>
2023-10-18 22:28:10 +0700

    change database

--- src/main/java/controler/MainServlet.java
+++ src/main/java/controler/MainServlet.java
@@ -1,44 +1,24 @@
 package controler;
 
-import service.DataConnector;
-import service.IUserDAO;
+import model.User;
 import service.UserDAOImpl;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.Properties;
+import java.util.List;
 
 @WebServlet(name = "UserServlet",value = "/main")
 public class MainServlet extends HttpServlet {
+    private UserDAOImpl userDAO ;
 
     @Override
-    public void init() throws ServletException{
-
-        ServletContext context = getServletContext();
-        InputStream input = context.getResourceAsStream("/WEB-INF/classes/config.properties");
-        Properties properties = new Properties();
-        try {
-            properties.load(input);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        String dbUrl = properties.getProperty("db.url");
-        String dbUsername = properties.getProperty("db.username");
-        String dbPassword = properties.getProperty("db.password");
-
-        try {
-            DataConnector.getConnection();
-        } catch (ClassNotFoundException | SQLException e) {
-            throw new RuntimeException(e);
-        }
+    public void init() throws ServletException {
+        userDAO = new UserDAOImpl();
     }
 
     @Override
@@ -48,6 +28,12 @@ public class MainServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        directToListView(req,resp);
+    }
+    public void directToListView(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        List<User> defaultListUser = userDAO.getAllUser();
+        session.setAttribute("defaultListUser", defaultListUser);
         req.getRequestDispatcher("/viewAdmin/user-list-view.jsp").forward(req,resp);
     }
 }
--- src/main/webapp/viewAdmin/user-list-view.jsp
+++ src/main/webapp/viewAdmin/user-list-view.jsp
@@ -1,8 +1,4 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<%@ page import="service.UserDAOImpl" %>
-<%@ page import="java.util.List" %>
-<%@ page import="model.User" %>
-<%@ page import="java.sql.SQLException" %>
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <!DOCTYPE html>
 <html>
@@ -19,26 +15,16 @@
                 <th>Password</th>
                 <th>Email</th>
                 <th>Phone</th>
-                <th>Birth</th>
-                <th>Avatar</th>
-                <th>Name</th>
-                <th>Address</th>
-                <th>Hobby</th>
                 <th>Permission</th>
             </tr>
-            <c:forEach var="user" items="${sessionScope.defaultListUser}">
+            <c:forEach var="user" items="${sessionScope.['defaultListUser']}">
                 <tr>
                     <td>${user.id}</td>
-                    <td>${user.id}</td>
-                    <td>${user.id}</td>
-                    <td>${user.id}</td>
-                    <td>${user.id}</td>
-                    <td>${user.id}</td>
-                    <td>${user.id}</td>
-                    <td>${user.id}</td>
-                    <td>${user.id}</td>
-                    <td>${user.id}</td>
-                    <td>${user.id}</td>
+                    <td>${user.username}</td>
+                    <td>${user.password}</td>
+                    <td>${user.email}</td>
+                    <td>${user.phone}</td>
+                    <td>${user.permssion}</td>
                 </tr>
             </c:forEach>
         </table>
